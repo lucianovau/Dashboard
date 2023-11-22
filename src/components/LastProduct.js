005f4179e83e7e9e0d499537f17fec76d53c71f2
lucianovau <lulinovau@gmail.com>
2023-11-22 11:33:28 -0300

    pruebas dashboard

---
+++ src/components/LastProduct.js
@@ -0,0 +1,59 @@
+import React, {useEffect, useState } from 'react'; 
+
+function LastProduct(){
+    const [data, setData] = useState(null);
+    const [idLast, setId] = useState(null);
+    const [img, setImg] = useState(null);
+
+	useEffect(()=>{
+		fetch('http://localhost:3030/api/products')
+		.then(response => response.json())
+        .then((data)=>{
+             return (data.products.pop())
+          })
+        .then((data)=>{
+            setId(data.id)
+        })
+	}, []);
+
+    useEffect(()=>{
+        
+        fetch(`http://localhost:3030/api/products/${idLast}`)
+        .then(response => response.json())
+        .then((data)=>{
+            setData(data)
+        })
+    }, [idLast]);
+
+    useEffect(()=>{
+        
+        fetch(`http://localhost:3030/api/products/${idLast}/img`)
+        .then(response => response.json())
+        .then((data)=>{
+            setImg(data)
+        })
+    }, [idLast]);
+    
+    return(
+        <React.Fragment>
+                {/*<!-- Last Product in DB -->*/}
+                <div className="col-lg-6 mb-4">
+                    <div className="card shadow mb-4">
+                        <div className="card-header py-3">
+                            <h5 className="m-0 font-weight-bold text-gray-800">Last product in Data Base</h5>
+                        </div>
+                        <div className="card-body">
+                            <h3>{data?.name}</h3>
+                            <div className="text-center">
+                                <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 40 +'rem'}} src= {img?`http://localhost:3030/${img[4]}`: null } alt=""/>
+                            </div>
+                            <p>{data?.descripcion}</p>
+                            <a className="btn btn-danger" target="_blank" rel="nofollow" href="/">View product detail</a>
+                        </div>
+                    </div>
+                </div>
+        </React.Fragment>
+	) 
+}
+
+export default LastProduct;
---
+++ src/components/Totals.js
@@ -0,0 +1,86 @@
+import React, {useEffect, useState } from 'react'; 
+
+function Totals(){
+	const [dataProd, setDataprod] = useState(null);
+
+	useEffect(()=>{
+		fetch('http://localhost:3030/api/products')
+		.then(response => response.json() )
+		.then((data)=>{
+			setDataprod(data)
+		})
+	}, [])
+
+    const [dataUser, setDatauser] = useState(null);
+
+	useEffect(()=>{
+		fetch('http://localhost:3030/api/users')
+		.then(response => response.json() )
+		.then((data)=>{
+			setDatauser(data)
+		})
+	}, [])
+
+    return(
+        <React.Fragment>
+				{/*<!-- Content Row Totals-->*/}
+					<div className="row">
+
+						{/*<!-- Total products -->*/}
+						<div className="col-md-4 mb-4">
+							<div className="card border-left-primary shadow h-100 py-2">
+								<div className="card-body">
+									<div className="row no-gutters align-items-center">
+										<div className="col mr-2">
+											<div className="text-xs font-weight-bold text-primary text-uppercase mb-1">Products in Data Base</div>
+											<div className="h5 mb-0 font-weight-bold text-gray-800">{dataProd?.count}</div>
+										</div>
+										<div className="col-auto">
+											<i className="fas fa-film fa-2x text-gray-300"></i>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						{/*<!-- Total users -->*/}
+						<div className="col-md-4 mb-4">
+							<div className="card border-left-success shadow h-100 py-2">
+								<div className="card-body">
+									<div className="row no-gutters align-items-center">
+										<div className="col mr-2">
+											<div className="text-xs font-weight-bold text-success text-uppercase mb-1">Total Users</div>
+											<div className="h5 mb-0 font-weight-bold text-gray-800">{dataUser?.count}</div>
+										</div>
+										<div className="col-auto">
+										<i className="fas fa-user fa-2x text-gray-300"></i>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						{/*<!-- Total Categorias -->*/}
+						<div className="col-md-4 mb-4">
+							<div className="card border-left-warning shadow h-100 py-2">
+								<div className="card-body">
+									<div className="row no-gutters align-items-center">
+										<div className="col mr-2">
+											<div className="text-xs font-weight-bold text-warning text-uppercase mb-1">Total Brands
+											</div>
+											<div className="h5 mb-0 font-weight-bold text-gray-800">5</div>
+										</div>
+										<div className="col-auto">
+											<i className="fas fa-award fa-2x text-gray-300"></i>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+			{/*<!-- End movies in Data Base -->*/}
+        </React.Fragment>
+	) 
+}
+
+export default Totals;
